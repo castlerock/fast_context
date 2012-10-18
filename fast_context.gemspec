21d17a6e758f235815192c3f59ba91ae4870b8a6
Nick Ewing <nick@nickewing.net>
2012-10-18 15:58:31 -0700

    Update for use with shoulda ~> 3.0

---
+++ fast_context.gemspec
@@ -0,0 +1,19 @@
+$LOAD_PATH << File.join(File.dirname(__FILE__), 'lib')
+
+Gem::Specification.new do |s|
+  s.name        = 'fast_context'
+  s.version     = "1.1.0"
+  s.platform    = Gem::Platform::RUBY
+  s.authors     = ["Pratik Naik", "Ed Schmalzle", "Nick Ewing"]
+  s.email       = %q{support@thoughtbot.com}
+  s.homepage    = %q{https://github.com/analog-analytics/fast_context}
+  s.summary     = %q{Make your shoulda contexts faster}
+  s.description = %q{Make your shoulda contexts faster}
+
+  s.files         = `git ls-files`.split("\n")
+  s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
+  s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
+  s.require_paths = ["lib"]
+
+  s.add_dependency("shoulda", "~> 3.0")
+end
--- lib/fast_context.rb
+++ lib/fast_context.rb
@@ -1,4 +1,3 @@
-puts "Loading fast_context"
 require 'shoulda/context'
 
 module ShouldaContextExtensions
@@ -11,7 +10,7 @@ module ShouldaContextExtensions
 
   def fast_context(name, &blk)
     @fast_subcontexts ||= []
-    @fast_subcontexts << Shoulda::FastContext.new(name, self, &blk)
+    @fast_subcontexts << Shoulda::Context::FastContext.new(name, self, &blk)
   end
 
   def build_with_fast_context
@@ -21,63 +20,65 @@ module ShouldaContextExtensions
   end
 
   def am_subcontext_with_fast_context?
-    parent.is_a?(Shoulda::Context) || parent.is_a?(Shoulda::FastContext)
+    parent.is_a?(Shoulda::Context::Context) || parent.is_a?(Shoulda::Context::FastContext)
   end
 end
 
 module Shoulda
-  class FastContext < Context
-    def create_test_from_should_hash
-      test_name = ["test:", full_name, "should", "run_fast"].flatten.join(' ').to_sym
+  module Context
+    class FastContext < Context
+      def create_test_from_should_hash
+        test_name = ["test:", full_name, "should", "run_fast"].flatten.join(' ').to_sym
 
-      if test_unit_class.instance_methods.include?(test_name.to_s)
-        warn "  * WARNING: '#{test_name}' is already defined"
-      end
+        if test_unit_class.instance_methods.include?(test_name.to_s)
+          warn "  * WARNING: '#{test_name}' is already defined"
+        end
 
-      context = self
-      test_unit_class.send(:define_method, test_name) do
-        @shoulda_context = context
-        @current_should = nil
-        begin
-          context.run_parent_setup_blocks(self)
-          context.shoulds.each do |s| 
-            @current_should = s
-            s[:before].bind(self).call if s[:before] 
+        context = self
+        test_unit_class.send(:define_method, test_name) do
+          @shoulda_context = context
+          @current_should = nil
+          begin
+            context.run_parent_setup_blocks(self)
+            context.shoulds.each do |s| 
+              @current_should = s
+              s[:before].bind(self).call if s[:before] 
+            end
+            context.run_current_setup_blocks(self)
+
+            context.shoulds.each {|should| should[:block].bind(self).call }
+          rescue Test::Unit::AssertionFailedError => e
+            error = Test::Unit::AssertionFailedError.new(["FAILED:", context.full_name, "should", "#{@current_should[:name]}:", e.message].flatten.join(' '))
+            error.set_backtrace e.backtrace
+            raise error
+          ensure
+            context.run_all_teardown_blocks(self)
           end
-          context.run_current_setup_blocks(self)
-
-          context.shoulds.each {|should| should[:block].bind(self).call }
-        rescue Test::Unit::AssertionFailedError => e
-          error = Test::Unit::AssertionFailedError.new(["FAILED:", context.full_name, "should", "#{@current_should[:name]}:", e.message].flatten.join(' '))
-          error.set_backtrace e.backtrace
-          raise error
-        ensure
-          context.run_all_teardown_blocks(self)
         end
       end
-    end
 
-    def build
-      create_test_from_should_hash
-      subcontexts.each {|context| context.build }
+      def build
+        create_test_from_should_hash
+        subcontexts.each {|context| context.build }
 
-      @fast_subcontexts ||= []
-      @fast_subcontexts.each {|f| f.build }
+        @fast_subcontexts ||= []
+        @fast_subcontexts.each {|f| f.build }
 
-      print_should_eventuallys
+        print_should_eventuallys
+      end
     end
   end
 end
 
 class ActiveSupport::TestCase
   def self.fast_context(name, &blk)
-    if Shoulda.current_context
-      Shoulda.current_context.fast_context(name, &blk)
+    if Shoulda::Context.current_context
+      Shoulda::Context.current_context.fast_context(name, &blk)
     else
-      context = Shoulda::FastContext.new(name, self, &blk)
+      context = Shoulda::Context::FastContext.new(name, self, &blk)
       context.build
     end
   end  
 end
 
-Shoulda::Context.send :include, ShouldaContextExtensions
+Shoulda::Context::Context.send :include, ShouldaContextExtensions
