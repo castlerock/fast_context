be6254f462f768f72455289e39c8a0f523f818b8
Sabarish Kumar R <skumar@crri.co.in>
2015-02-11 16:17:24 +0530

    Updated to work with shoulda with merges

--- Rakefile
+++ Rakefile
@@ -1,12 +1,13 @@
 begin
   require 'jeweler'
   Jeweler::Tasks.new do |gemspec|
-    gemspec.name = "fast_context"
-    gemspec.summary = "This is a gem version of the fast_context plugin"
-    gemspec.email = "adam@pohorecki.pl"
-    gemspec.homepage = "http://github.com/psyho/fast_context"
-    gemspec.authors = ["Pratik Naik"]
-    gemspec.add_dependency 'shoulda'
+    gemspec.name = "shoulda_fast_context"
+    gemspec.description = "make your shoulda context faster with fast_context"
+    gemspec.description = "This is a shoulda extension with fast_context which runs setup only once inside the context"
+    gemspec.email = "supportdev@crri.co.in"
+    gemspec.homepage = "http://github.com/castlerock/fast_context"
+    gemspec.authors     = ["Pratik Naik", "Ed Schmalzle", "Nick Ewing", "Sabarish Kumar R"]
+    gemspec.add_dependency 'shoulda', "~> 3.0"
   end
 rescue LoadError
   puts "Jeweler not available. Install it with: gem install jeweler"
--- lib/fast_context.rb
+++ lib/fast_context.rb
@@ -28,16 +28,17 @@ module ShouldaContextExtensions
 end
 
 module Shoulda
-  class FastContext < Context
-    def test_method_name
-      joined_should_name = shoulds.collect{ |should_hash| should_hash[:name] }.join(' and ')
-      test_name = ["test", full_name, "should", joined_should_name].flatten.join('_')
-      test_name = test_name.gsub(' ', '_').gsub(/[^a-zA-Z0-9_?!]/, '').gsub(/__+/, '_').to_sym
-      return test_name
-    end
+  module Context
+    class FastContext < Context
+      def test_method_name
+        joined_should_name = shoulds.collect{ |should_hash| should_hash[:name] }.join(' and ')
+        test_name = ["test", full_name, "should", joined_should_name].flatten.join('_')
+        test_name = test_name.gsub(' ', '_').gsub(/[^a-zA-Z0-9_?!]/, '').gsub(/__+/, '_').to_sym
+        return test_name
+      end
 
-    def create_test_from_should_hash
-      test_name = test_method_name
+      def create_test_from_should_hash
+        test_name = test_method_name
 
         if test_unit_class.instance_methods.include?(test_name.to_s)
           warn "  * WARNING: '#{test_name}' is already defined"
@@ -55,24 +56,25 @@ module Shoulda
             end
             context.run_current_setup_blocks(self)
 
-          context.shoulds.each {|should| should[:block].bind(self).call }
-        rescue Test::Unit::AssertionFailedError => e
-          error = Test::Unit::AssertionFailedError.new(["test:", context.full_name, "should", "#{@current_should[:name]}:", e.message].flatten.join(' '))
-          error.set_backtrace e.backtrace
-          raise error
-        ensure
-          context.run_all_teardown_blocks(self)
+            context.shoulds.each {|should| should[:block].bind(self).call }
+          rescue Test::Unit::AssertionFailedError => e
+            error = Test::Unit::AssertionFailedError.new(["test:", context.full_name, "should", "run_fast", "#{@current_should[:name]}:", e.message].flatten.join(' '))
+            error.set_backtrace e.backtrace
+            raise error
+          ensure
+            context.run_all_teardown_blocks(self)
+          end
         end
-      end
 
-      def build
-        create_test_from_should_hash
-        subcontexts.each {|context| context.build }
+        def build
+          create_test_from_should_hash
+          subcontexts.each {|context| context.build }
 
-        @fast_subcontexts ||= []
-        @fast_subcontexts.each {|f| f.build }
+          @fast_subcontexts ||= []
+          @fast_subcontexts.each {|f| f.build }
 
-        print_should_eventuallys
+          print_should_eventuallys
+        end
       end
     end
   end
@@ -82,10 +84,10 @@ module FastContextMethods
   def self.included(base)
     base.class_eval do
       def self.fast_context(name, &blk)
-        if Shoulda.current_context
-          Shoulda.current_context.fast_context(name, &blk)
+        if Shoulda::Context.current_context
+          Shoulda::Context.current_context.fast_context(name, &blk)
         else
-          context = Shoulda::FastContext.new(name, self, &blk)
+          context = Shoulda::Context::FastContext.new(name, self, &blk)
           context.build
         end
       end
@@ -112,4 +114,4 @@ ActiveSupport::TestCase.send(:include, FastContextMethods) if defined? ActiveSup
 Test::Unit::TestCase.send(:include, FastContextMethods) if defined? Test::Unit::TestCase
 Test::Unit::TestCase.send(:include, TestUnitOutputHelpers)
 
-Shoulda::Context.send :include, ShouldaContextExtensions
+Shoulda::Context::Context.send :include, ShouldaContextExtensions
